eeacfd5c72c7ba4319e5a194fc31405efd5df44f
Pritam <pritambanikroy@gmail.com>
2025-01-22 11:16:07 +0530

    Updated

--- src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
@@ -3,7 +3,7 @@ package com.in28minutes.learn_spring_framework;
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.PacmanGame;
 
-public class App01GammingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
@@ -0,0 +1,48 @@
+package com.in28minutes.learn_spring_framework.helloworld;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		
+		//1:launch a spring context
+		try(var context =
+				new AnnotationConfigApplicationContext
+				        (HelloWorldConfiguration.class)) {
+			
+			//2:configure the things that we want spring to manage -@Configuration
+		    //HelloWorldConfiguration -@Configuraton
+			//name -@Bean
+			
+			//3: Retrieving Beans managed by Spring
+			System.out.println(context.getBean("name"));
+			
+			System.out.println(context.getBean("age"));
+			
+			System.out.println(context.getBean("person"));
+			
+			System.out.println(context.getBean("person2MethodCall"));
+			
+			System.out.println(context.getBean("person3Parameters"));
+			
+			System.out.println(context.getBean("address2"));
+			
+			System.out.println(context.getBean(Person.class));
+			
+			System.out.println(context.getBean(Address.class));
+			
+			System.out.println(context.getBean("person5Qualifier"));
+			
+			
+			//System.out.println
+//			Arrays.stream(context.getBeanDefinitionNames())
+//			    .forEach(System.out::println);
+			
+		}
+		
+		
+		
+	}
+
+}
